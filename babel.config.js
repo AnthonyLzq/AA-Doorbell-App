a6f0ff3c31aa214218d7a426962831701c0004a4
AnthonyLzq <sluzquinosa@uni.pe>
2022-10-16 18:43:50 -0500

    feat: configured centralized exports using index file in each folder

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
             screens: './screens',
             navigation: './navigation',
             hooks: './hooks',
-            constants: './constants',
+            utils: './utils',
             components: './components',
             assests: './assests',
             types: './types'
--- components/Themed.tsx
+++ components/Themed.tsx
@@ -5,8 +5,8 @@
 
 import { Text as DefaultText, View as DefaultView } from 'react-native'
 
-import Colors from 'constants/Colors'
-import useColorScheme from 'hooks/useColorScheme'
+import { Colors } from 'utils'
+import { useColorScheme } from 'hooks'
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
